d52ced8d84a73910c49aa29eec083371f04741a0
Koray Erdem <developerkoray@gmail.com>
2021-06-17 22:22:34 +0300

    downloaded dependencies and router file

---
+++ src/pages/Detail/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const Detail = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Detail;
