511a3d567b61ce36a72d24a7528f68ede22564b3
Koray Erdem KILIÇ <developerkoray@gmail.com>
2021-08-12 16:41:12 +0300

    added TextInput component

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import TextInput from '../../components/Input';
 
+import styles from './Login.style';
 const Login = () => {
   return (
-    <SafeAreaView>
-      <Text>Login</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logo_container}>
+      <TextInput placeholder="Kullanıcı adı giriniz" />
+      <TextInput placeholder="Şifrenizi giriniz" />
+        <Text>Dükkan</Text>
+      </View>
+      <View style={styles.body_container}>
+        <Text>Dükkan</Text>
+      </View>
     </SafeAreaView>
   );
 };
